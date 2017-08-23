8450ff38b9bb4c7a08f100dcd65a1b257874f7b7
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-23 08:45:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lista21082017ex08
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int quantidade;
+            decimal valorTotal;
+
+            Console.WriteLine("Digite a quantidade de maçãs compradas: ");
+            quantidade = Convert.ToInt32(Console.ReadLine());
+
+            if(quantidade < 12){
+                valorTotal = quantidade * (decimal)1.3;
+            } else {
+                valorTotal = quantidade;
+            }
+
+            Console.WriteLine("O valor total é: R$ {0:###,##0.00}", valorTotal);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Lista21082017ex08 C# Curso Programação de Dispositivos Móveis SENAC
